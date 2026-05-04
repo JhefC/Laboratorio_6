d22853a5c53a267024ebcf8c2edd4d1b0f6e702a
Jheferson Cortez <jhefcortezcubas@gmail.com>
2026-05-04 08:25:39 -0500

    v1: Sistema base con variable global

---
+++ Laboratorio_6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Programa
+{
+    static int LIMITE_GLOBAL = 80;
+
+    static void MostrarEncabezado()
+    {
+        Console.WriteLine("==== SISTEMA INDUSTRIAL ====");
+    }
+
+    static bool EvaluarTemperatura(double temp)
+    {
+        if (temp > LIMITE_GLOBAL)
+            return true;
+        return false;
+    }
+
+    static void Main()
+    {
+        MostrarEncabezado();
+        Console.Write("Ingrese temperatura: ");
+        double temp = double.Parse(Console.ReadLine());
+
+        if (EvaluarTemperatura(temp))
+            Console.WriteLine("ALERTA");
+        else
+            Console.WriteLine("NORMAL");
+    }
+}
